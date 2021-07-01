cc44a3884c1243b17afef39229d4eef7c8681603
stas-yushkov <stas.yushkov@gmail.com>
2021-07-01 04:21:57 +0300

    adding some color vars

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
 import '../node_modules/modern-normalize/modern-normalize.css';
 import './sass/main.scss';
+
+// https://www.figma.com/file/oBwNdA0QCSTNdPEB4frMP8/Web-designer-portfolio-(Copy)-(Copy)?node-id=3130%3A18
